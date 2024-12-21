6b3bea68f4b6a6f89bf1814da78d94d93171bf26
Mike Coopersmith <mikecoop83@gmail.com>
2024-12-21 15:53:02 -0500

    better parsing of blocks

--- lib/pieces.go
+++ lib/pieces.go
@@ -5,34 +5,44 @@ import (
 	"strings"
 )
 
-//go:embed pieces.txt
-var piecesStr string
+var AllPieces []Piece
 
-var AllPieces = parsePieces(piecesStr)
-
-func parsePieces(input string) []Piece {
-	pieces := make([]Piece, 0, 2)
-	blocksStr := strings.Split(input, ".")
-
-	for _, blockStr := range blocksStr {
-		var shape [][]byte
-		rows := strings.Split(blockStr, "\n")
-		for _, row := range rows {
-			var shapeRow []byte
-			for _, char := range row {
-				if char == '#' {
-					shapeRow = append(shapeRow, 1)
-				} else {
-					shapeRow = append(shapeRow, 0)
-				}
-			}
-			if len(shapeRow) > 0 {
-				shape = append(shape, shapeRow)
-			}
-		}
-		if len(shape) > 0 {
-			pieces = append(pieces, Piece{Shape: shape})
-		}
+func init() {
+	AllPieces = []Piece{
+		parsePiece("#"),
+		parsePiece("##"),
+		parsePiece("###"),
+		parsePiece("####"),
+		parsePiece("#####"),
+		parsePiece("##\n##"),
+		parsePiece("###\n###\n###"),
+		parsePiece("###\n #"),
+		parsePiece("#\n#\n##"),
+		parsePiece(" #\n #\n##"),
+		parsePiece(" ##\n##"),
+		parsePiece("##\n ##"),
+		parsePiece("##\n #"),
+		parsePiece("#\n##"),
 	}
-	return pieces
+}
+func parsePiece(pieceStr string) Piece {
+	rows := strings.Split(pieceStr, "\n")
+	shape := make([][]byte, 0, len(rows))
+	for _, row := range rows {
+		if len(row) == 0 {
+			continue
+		}
+		pieceRow := make([]byte, 0, len(row))
+		for _, c := range row {
+			if c == ' ' {
+				pieceRow = append(pieceRow, 0)
+			} else {
+				pieceRow = append(pieceRow, 1)
+			}
+		}
+		shape = append(shape, pieceRow)
+	}
+	return Piece{
+		Shape: shape,
+	}
 }
--- lib/pieces_test.go
+++ lib/pieces_test.go
@@ -7,31 +7,5 @@ import (
 )
 
 func TestParsePieces(t *testing.T) {
-	piecesStr := `
-###
- # 
-.
-###
-###
-###
-`
-	pieces := parsePieces(piecesStr)
-	require.Equal(
-		t,
-		[]Piece{
-			{
-				Shape: [][]byte{
-					{1, 1, 1},
-					{0, 1, 0},
-				},
-			},
-			{
-				Shape: [][]byte{
-					{1, 1, 1},
-					{1, 1, 1},
-					{1, 1, 1},
-				},
-			},
-		}, pieces,
-	)
+	require.Equal(t, Piece{Shape: [][]byte{{0, 1, 0}}}, parsePiece(" # "))
 }
