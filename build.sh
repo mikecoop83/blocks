50ae3a2486d658e125bf7c70620636757feafa23
Mike Coopersmith <mikecoop83@gmail.com>
2024-12-26 20:21:41 -0500

    index.html

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ mkdir -p out/
 rm -f out/*
 env GOOS=js GOARCH=wasm go build -o out/blocks.wasm github.com/mikecoop83/blocks
 cp resources/wasm_exec.js out/
-cp resources/blocks.html out/
+cp resources/*.html out/
