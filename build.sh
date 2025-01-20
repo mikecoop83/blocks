ee9b5345fac7c3975ceb5930953b492abf34b6fd
Mike Coopersmith <mikecoop83@gmail.com>
2025-01-20 15:48:37 -0500

    stupid

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if ! [ -x "$(command -v go)" ]; then
   echo "Installing Go for Linux..."
   curl -fsSL https://go.dev/dl/go1.23.4.linux-amd64.tar.gz -o go.tar.gz
   tar -xzf go.tar.gz
-  echo PATH=$PATH:"$(pwd)/go/bin"
+  export PATH=$PATH:$(pwd)/go/bin
 fi
 
 mkdir -p out/
