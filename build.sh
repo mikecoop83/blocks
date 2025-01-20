d8a1cdce5d163e3fded32130f452024c2ac0a882
Mike Coopersmith <mikecoop83@gmail.com>
2025-01-20 15:44:08 -0500

    does this help?

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
+# Install Go if not already installed
+if ! [ -x "$(command -v go)" ]; then
+  echo "Installing Go for Linux..."
+  curl -fsSL https://go.dev/dl/go1.23.4.linux-amd64.tar.gz -o go.tar.gz
+  tar -xzf go.tar.gz
+fi
+
 mkdir -p out/
 rm -f out/*
-env GOOS=js GOARCH=wasm go build -o out/blocks.wasm github.com/mikecoop83/blocks
-
+env GOOS=js GOARCH=wasm go/bin/go build -o out/blocks.wasm github.com/mikecoop83/blocks
