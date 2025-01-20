3700bd6201037a0f7e42b7114d1d8e6ec04c60e7
Mike Coopersmith <mikecoop83@gmail.com>
2025-01-20 12:22:55 -0500

    auth test

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"math/rand"
+	"syscall/js"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/mikecoop83/blocks/game"
@@ -23,9 +25,19 @@ func main() {
 		gameID = rand.Uint64()
 	}
 
+	js.Global().Set("wasmCallback", js.FuncOf(handleToken))
+
 	// Run the game.
 	err = ebiten.RunGame(game.New(gameID, updateGameID))
 	if err != nil {
 		panic(err)
 	}
+}
+func handleToken(this js.Value, args []js.Value) interface{} {
+	if len(args) > 0 {
+		token := args[0].String()
+		fmt.Println("Received Google ID Token:", token)
+		// Use the token to authenticate the player in your backend or game logic
+	}
+	return nil
 }
