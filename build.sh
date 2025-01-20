61b6111711af700a1af19efc392fddb6c5a161d8
Mike Coopersmith <mikecoop83@gmail.com>
2025-01-20 15:42:07 -0500

    boooo

--- build.sh
+++ build.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 set -e
 
-# Install Go if not already installed
-if ! [ -x "$(command -v go)" ]; then
-  echo "Installing Go for Linux..."
-  curl -fsSL https://go.dev/dl/go1.23.4.linux-amd64.tar.gz -o go.tar.gz
-  tar -xzf go.tar.gz
-  export PATH=$PATH:$(pwd)/go/bin
-fi
-
 mkdir -p out/
 rm -f out/*
 env GOOS=js GOARCH=wasm go build -o out/blocks.wasm github.com/mikecoop83/blocks
-cp resources/wasm_exec.js out/
-cp resources/*.html out/
-cp resources/*.png out/
+
